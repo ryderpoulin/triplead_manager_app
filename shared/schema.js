62eb91e999408ca2978a506b86b664aadd5fbf23
ryderpoulin <ryder.poulin@icloud.com>
2025-11-10 20:08:51 -0800

    schemaupdate

---
+++ shared/schema.js
@@ -0,0 +1,19 @@
+// Dummy runtime stub so Node can resolve @shared/schema.js
+export const Trip = {};
+export const Signup = {};
+export const TripsResponse = {};
+export const SignupsResponse = {};
+export const RandomizeResponse = {};
+export const ApproveRandomizationResponse = {};
+export const AddFromWaitlistResponse = {};
+export const DropParticipantResponse = {};
+export const randomizeRequestSchema = {};
+export const approveRandomizationRequestSchema = {};
+export const addFromWaitlistRequestSchema = {};
+export const addDriverRequestSchema = {};
+export const addNonDriverRequestSchema = {};
+export const reAddParticipantRequestSchema = {};
+export const dropParticipantRequestSchema = {};
+export const User = {};
+export const InsertUser = {};
+export default {};
