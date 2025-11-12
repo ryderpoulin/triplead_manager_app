99df9ff4bd39882de84d9009f0184985d39eaa07
ryderpoulin <ryder.poulin@icloud.com>
2025-11-12 08:51:38 -0800

    boom

--- server/vite.ts
+++ server/vite.ts
@@ -75,7 +75,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.join(process.cwd(), "client", "dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
